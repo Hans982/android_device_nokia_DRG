2be60042cf8aeb0fdf93598eb4d7e2df1aeecbf1
Hans98 <142167463+Hans982@users.noreply.github.com>
2025-02-03 17:29:45 +0700

    Update lineage_DRG.mk

--- lineage_DRG.mk
+++ lineage_DRG.mk
@@ -3,8 +3,25 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
+ALLOW_MISSING_DEPENDENCIES := true
+TARGET_DISABLE_EPPE := true
+BUILD_BROKEN_DUP_RULES := true
+DISABLE_ARTIFACT_PATH_REQUIREMENTS := true
+
 # Device
 $(call inherit-product, device/nokia/DRG/device.mk)
+
+# Matrixx flags
+MATRIXX_CHIPSET := SDM660
+MATRIXX_BATTERY := 3060mAh
+MATRIXX_DISPLAY := 1080x2280
+MATRIXX_MAINTAINER := Hansss!!!
+
+# Matrixx Gapps
+BUILD_GOOGLE_CONTACTS := true
+BUILD_GOOGLE_DIALER := true
+BUILD_GOOGLE_MESSAGE := true
+WITH_GMS := false
 
 BUILD_FINGERPRINT := Nokia/Dragon_00WW/DRG_sprout:10/QKQ1.190828.002/00WW_4_150:user/release-keys
 PRODUCT_BRAND := Nokia
